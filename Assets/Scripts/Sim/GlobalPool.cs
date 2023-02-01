49e8920692892737807753664b2fd3709c4d4b35
jaden <cep450@nyu.edu>
2023-02-01 15:49:05 -0500

    organized scripts folder

--- Assets/Scripts/Sim/GlobalPool.cs
+++ Assets/Scripts/Sim/GlobalPool.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Agent : MonoBehaviour
+public class GlobalPool : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
