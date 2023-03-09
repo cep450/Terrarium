7d52bb4787ea82e8695e4b3e18283b9d1400ff16
jaden <cep450@nyu.edu>
2023-03-08 23:08:36 -0500

    was a dummy and didn't merge the presentation changes

--- Assets/ScriptableObjects/Processes/HexProcess.cs
+++ Assets/ScriptableObjects/Processes/HexProcess.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HexProcess : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public struct ResourceUsed {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string name;
+    public int id;
+    public bool isConsumed;         //consumed or just required to be there?
+    public bool hexDiesIfNotMet;    //if this resource isnt there, hex will convert to death hex
+    public bool consumesGlobal;     //consumes from global pool or local hex?
+
+}
+
+public struct ResourceProduced {
+
+    public string name;
+    public int id;
+    public int amount;
+    public int limitOnHex;      //will not produce more than this amount onto this tile
+    public bool producesGlobal; //produce to local hex or global pool?
+
+}
+
+public class HexProcess : ScriptableObject
+{
+    
+    //Evolution of SimHex ScriptableObject. 
+    //Multiple processes can be on a hex. 
+    //Multiple hexes can use the same process. 
+
+    public int ticks;       //this Process will attempt every x ticks.
+
+    [SerializeField] public ResourceUsed [] rr;
+    [SerializeField] public ResourceProduced [] rp;
+
+
 }
