55ceffa5ee0aafa32f462957a110a696be111e6c
Zeru Hu <green2onion@gmail.com>
2023-03-22 16:44:48 -0400

    total satisfaction set to least satisfied necessity

--- Assets/Scripts/Sim/SimHex.cs
+++ Assets/Scripts/Sim/SimHex.cs
@@ -28,7 +28,7 @@ public class SimHex
 	//go up to 255
 	public int[] resourcesHas = new int[Sim.resources.Length]; //this changes 
 
-	SimHex [] neighbors;
+	SimHex[] neighbors;
 
 	//for tracking if stuff was satisfied the last tick 
 	int currentTick = -1;
@@ -46,10 +46,12 @@ public class SimHex
 		ChangeType(t);
 	}
 
-	public void LoadNeighbors() {
+	public void LoadNeighbors()
+	{
 		List<Cube> neighborCubes = Sim.hexMap.grid.Neighbors(this.cube);
 		neighbors = new SimHex[neighborCubes.Count];
-		for(int i = 0; i < neighborCubes.Count; i++) {
+		for (int i = 0; i < neighborCubes.Count; i++)
+		{
 			neighbors[i] = neighborCubes[i].simHex;
 		}
 	}
@@ -57,18 +59,21 @@ public class SimHex
 	//Called when this type changes type.
 	public void ChangeType(SimHexType newType)
 	{
-		if(newType == null) {
+		if (newType == null)
+		{
 			Debug.LogError("ERR: tried to change type to null");
 			return;
 		}
 
-		if(type != null) {
-			if(type.Equals(newType)) {
+		if (type != null)
+		{
+			if (type.Equals(newType))
+			{
 				Debug.LogError("tried to change a " + type.name + " tile to its own type");
 				return; //also prevents adding starting resources
 			}
 
-			Debug.Log("type changed at " + cube + "from " + type.name + " to " + newType.name);
+			//Debug.Log("type changed at " + cube + "from " + type.name + " to " + newType.name);
 			Tracker.AddedHex(newType.id, 1);
 			Tracker.RemovedHex(type.id, 1);
 		}
@@ -86,24 +91,30 @@ public class SimHex
 	}
 
 	//add a resource to this hex
-	public void AddResource(int id, int amount) {
+	public void AddResource(int id, int amount)
+	{
 		resourcesHas[id] += amount;
 		Tracker.AddedRes(id, amount);
 	}
-	public void AddResource(string name, int amount) {
+	public void AddResource(string name, int amount)
+	{
 		AddResource(Resource.IdByName(name), amount);
 	}
-	public bool HasResource(int id, int amount) {
+	public bool HasResource(int id, int amount)
+	{
 		return resourcesHas[id] >= amount;
 	}
-	public void HasResource(string name, int amount) {
+	public void HasResource(string name, int amount)
+	{
 		HasResource(Resource.IdByName(name), amount);
 	}
 	//consume a resource from this hex
-	public void ConsumeResource(string name, int amount) {
+	public void ConsumeResource(string name, int amount)
+	{
 		ConsumeResource(Resource.IdByName(name), amount);
 	}
-	public void ConsumeResource(int id, int amount) {
+	public void ConsumeResource(int id, int amount)
+	{
 		resourcesHas[id] -= amount;
 		Tracker.UsedRes(id, amount);
 	}
@@ -113,11 +124,14 @@ public class SimHex
 	public void InputTick(int tickNum)
 	{
 		InputStatus result = CheckInputs();
-		if(result == InputStatus.Satisfied) {
+		if (result == InputStatus.Satisfied)
+		{
 			ConsumeInputs();
 			currentTick = tickNum;
 			inputsSatisfied = true;
-		} else if(result == InputStatus.NotSatisfiedDies) {
+		}
+		else if (result == InputStatus.NotSatisfiedDies)
+		{
 			Die();
 		}
 	}
@@ -139,7 +153,8 @@ public class SimHex
 		}
 	}
 
-	public enum InputStatus {
+	public enum InputStatus
+	{
 		NotSatisfiedDies, NotSatisfied, Satisfied
 	}
 	public InputStatus CheckInputs()
@@ -156,28 +171,37 @@ public class SimHex
 
 			int sum = 0;
 
-			if(!rr.local) { //drawing from global 
+			if (!rr.local)
+			{ //drawing from global 
 
 				sum = GlobalPool.resources[rr.id];
 
-			} else { //drawing from local- self and neighbors 
+			}
+			else
+			{ //drawing from local- self and neighbors 
 
 				//factor in self 
 				sum += resourcesHas[rr.id];
 
 				//factor in neighbors, if applicable 
 				//TODO for now, only possible to check immediate neighbors, radius needs more
-				if(rr.radius > 0) {
-					foreach(SimHex neighbor in neighbors) {
+				if (rr.radius > 0)
+				{
+					foreach (SimHex neighbor in neighbors)
+					{
 						sum += neighbor.resourcesHas[rr.id];
 					}
 				}
 			}
-			
-			if (sum < rr.amount) {
-				if(rr.diesIfNotNet) {
+
+			if (sum < rr.amount)
+			{
+				if (rr.diesIfNotNet)
+				{
 					return InputStatus.NotSatisfiedDies; //dies, end 
-				} else {
+				}
+				else
+				{
 					status = InputStatus.NotSatisfied; //continue checking if it might die
 				}
 			}
@@ -194,11 +218,14 @@ public class SimHex
 			if (rr.isConsumed)
 			{
 
-				if(!rr.local) { //consumes from global pool
+				if (!rr.local)
+				{ //consumes from global pool
 
 					GlobalPool.Consume(rr.id, rr.amount);
 
-				} else if(rr.radius > 0) { //consumes from self and neighbors
+				}
+				else if (rr.radius > 0)
+				{ //consumes from self and neighbors
 
 					int amount = rr.amount;
 
@@ -206,19 +233,26 @@ public class SimHex
 
 					//cycle thru self and neighbors consuming resources til satisfied
 					//TODO start i at a random neighbor index?
-					for(int i = 0; amount > 0; i++) {
-						if(i == 0) {
+					for (int i = 0; amount > 0; i++)
+					{
+						if (i == 0)
+						{
 							this.ConsumeResource(rr.id, 1);
-						} else {
+						}
+						else
+						{
 							neighbors[i - 1].ConsumeResource(rr.id, 1);
 						}
 						amount--;
-						if(i > neighbors.Length) {
+						if (i > neighbors.Length)
+						{
 							i = 0;
 						}
 					}
-					
-				} else { //just consumes from self
+
+				}
+				else
+				{ //just consumes from self
 
 					ConsumeResource(rr.id, rr.amount);
 
@@ -237,15 +271,18 @@ public class SimHex
 		foreach (ResProduced rp in type.resourcesProduced)
 		{
 
-			if(rp.isHex) {
+			if (rp.isHex)
+			{
 				//flipping a hex
 
 				//TODO we'll need rules about priority. maybe use their order in the hextype []
 				//TODO also factor in larger radiuses
 
 				//if we only do this every x ticks
-				if(rp.tickMod > 0) {
-					if(tickNum % rp.tickMod != 0) {
+				if (rp.tickMod > 0)
+				{
+					if (tickNum % rp.tickMod != 0)
+					{
 						//doesn't produce this tick
 						continue;
 					}
@@ -255,26 +292,33 @@ public class SimHex
 				int rand = Random.Range(0, neighbors.Length);
 
 				//if we try every neighbor
-				if(rp.tryAll) {
+				if (rp.tryAll)
+				{
 					int changeIndex = -1;
-					for(int i = 0; i < neighbors.Length; i++) {
+					for (int i = 0; i < neighbors.Length; i++)
+					{
 						int index = (rand + i) % neighbors.Length;
 						SimHex neighbor = neighbors[index]; //wrap
 
 						//if we can change it
-						if(System.Array.IndexOf<string>(rp.changes, neighbor.type.name) >= 0) {
+						if (System.Array.IndexOf<string>(rp.changes, neighbor.type.name) >= 0)
+						{
 							changeIndex = index;
 							break;
 						}
 					}
-					if(changeIndex == -1) {
+					if (changeIndex == -1)
+					{
 						//no valid hex to flip 
 						continue;
 					}
 					rand = changeIndex;
-				} else {
+				}
+				else
+				{
 					//just the one 
-					if(!(System.Array.IndexOf<string>(rp.changes, neighbors[rand].type.name) >= 0)) {
+					if (!(System.Array.IndexOf<string>(rp.changes, neighbors[rand].type.name) >= 0))
+					{
 						//no valid hex to flip here 
 						continue;
 					}
@@ -284,38 +328,54 @@ public class SimHex
 				//TODO probably have to factor in if multiple change types affect the same tile in the same frame
 				//chekcing the type might work?
 
-			} else {
+			}
+			else
+			{
 				//producing resources 
 
-				if(!rp.local) { //producing to global pool 
+				if (!rp.local)
+				{ //producing to global pool 
 
-					if(rp.cap > 0) { //don't produce over cap 
+					if (rp.cap > 0)
+					{ //don't produce over cap 
 						int diff = rp.cap - GlobalPool.resources[rp.id];
 						GlobalPool.Add(rp.id, Mathf.Min(rp.amount, diff));
-					} else {
+					}
+					else
+					{
 						GlobalPool.Add(rp.id, rp.amount);
 					}
 
-				} else { //producing to self and neighbors 
+				}
+				else
+				{ //producing to self and neighbors 
 
-					if(rp.cap > 0) { //don't produce over cap 
+					if (rp.cap > 0)
+					{ //don't produce over cap 
 						int diff = rp.cap - resourcesHas[rp.id];
 						AddResource(rp.id, Mathf.Min(rp.amount, diff));
-					} else {
+					}
+					else
+					{
 						AddResource(rp.id, rp.amount);
 					}
 
 					//TODO what if a tile is full up?
 					//TODO is the amount per tile or does it get spread between them?
-				
-					//TODO account for higher radius and falloff 
-					if(rp.radius > 0) {
-						foreach(SimHex neighbor in neighbors) {
 
-							if(rp.cap > 0) { //don't produce over cap 
+					//TODO account for higher radius and falloff 
+					if (rp.radius > 0)
+					{
+						foreach (SimHex neighbor in neighbors)
+						{
+
+							if (rp.cap > 0)
+							{ //don't produce over cap 
 								int diff = rp.cap - neighbor.resourcesHas[rp.id];
 								neighbor.AddResource(rp.id, Mathf.Min(rp.amount, diff));
-							} else {
+							}
+							else
+							{
 								neighbor.AddResource(rp.id, rp.amount);
 							}
 						}
@@ -326,12 +386,13 @@ public class SimHex
 				//visualHex.VisualUpdate(); //TODO when adding sprite effects
 				//TODO maybe put this in AddResource and Consume so it auto updates?
 				//visual update should check its hex's resources if it meets threshes for showing variuos stuff 
-				
+
 			}
 		}
 	}
 
-	public void Die() {
+	public void Die()
+	{
 
 		//todo it could also leave behind resources on death?
 
