d814ecae9dde051c2ec1e8361796141baeb0086e
jaden <cep450@nyu.edu>
2023-04-03 15:06:24 -0400

    custom cursor code setup

--- Assets/ScriptableObjects/BuildYOnX/BuildXOnYUI.cs
+++ Assets/ScriptableObjects/BuildYOnX/BuildXOnYUI.cs
@@ -14,40 +14,6 @@ public struct OnInfo {
     public int numTicks;
 }
 
-public class BuildUIEntry : MonoBehaviour {
-
-    public SimHexType type;
-
-    //Populates itself based on the data in its type.
-    //For Build 
-    public void Construct(OnInfo info) {
-        
-        Construct(info.type);
-
-        //TODO 
-
-        //set number of ticks 
-        //set description 
-
-    }  
-    //For On  
-    public void Construct(SimHexType t) {
-
-        type = t;
-        
-        //set color based on type 
-        //set icon based on type 
-    }
-
-    public void Expand() {
-
-    }
-
-    public void Contract() {
-
-    }
-}
-
 public class BuildXOnYUI : MonoBehaviour {
 
     [SerializeField] GameObject buttonQueue;
@@ -142,12 +108,4 @@ public class BuildXOnYUI : MonoBehaviour {
 
         //AgentDirector.AddTask(typeTarget, typeDestination, ticks);
     }
-}
-
-public class BuildXOnYInfo : MonoBehaviour {
-
-    public SimHexType type;
-
-
-
 }
