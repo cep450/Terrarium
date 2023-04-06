d2c0e3cd7e33f0d0c93774544be88fa6d9c872f5
jaden <cep450@nyu.edu>
2023-04-06 15:11:47 -0400

    icons in hover, scale arrows instead of icons

--- Assets/Scripts/UI/ResourceChangeIcon.cs
+++ Assets/Scripts/UI/ResourceChangeIcon.cs
@@ -8,6 +8,8 @@ public class ResourceChangeIcon : MonoBehaviour
     [SerializeField] Image renderer; //TODO numbers or counts of resource or size? size could be useful
     [SerializeField] Image upArrow, downArrow;
 
+    static float scaleMult = 0.5f;
+
     public void Fill(Sprite sprite, bool produces, bool consumes, int amount = 1) {
 
         Empty();
@@ -16,17 +18,21 @@ public class ResourceChangeIcon : MonoBehaviour
         renderer.sprite = sprite;
 
         if(amount < 1) amount = 1;
-        renderer.transform.localScale = Vector3.one * amount; //for now, showing amount with scale
+        Vector3 amountScale = Vector3.one * (((amount - 1) * scaleMult) + 1);
 
+        //TODO this could also be like, a triple arrow instead of a single arrow
         if(produces) {
             upArrow.enabled = true;
+            upArrow.transform.localScale = amountScale;
         } else if(consumes) {
             downArrow.enabled = true;
+            downArrow.transform.localScale = amountScale;
         }
     }
 
     public void Empty() {
-        renderer.transform.localScale = Vector3.one;
+        upArrow.transform.localScale = Vector3.one;
+        downArrow.transform.localScale = Vector3.one;
         renderer.enabled = false;
         upArrow.enabled = false;
         downArrow.enabled = false;
